ab2f30be2a7b75ced7646a49d611ed4713c0028a
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2022-11-01 18:38:03 -0300

    Generated grid in memory

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def hash_input(input) do
@@ -21,8 +22,28 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  def build_grid(%Identicon.Image{hex: hex_list = _}) do
-    hex_list
-    |> Enum.chunk_every(3, 3, :discard)
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid =
+      hex_list
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
+
+  def mirror_row(row) do
+    [first, second, _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn {code, _index} ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
 end
